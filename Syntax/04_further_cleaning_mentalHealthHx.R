21d6ae2f0f3554bbec4142920261c7215106c310
Jeremy Eberle <jwe4ec@virginia.edu>
2021-08-13 16:19:04 -0400

    Clean mental health history variables

--- Syntax/04_further_cleaning_mentalHealthHx.R
+++ Syntax/04_further_cleaning_mentalHealthHx.R
@@ -22,14 +22,14 @@ if(current_R_version != script_R_version) {
 # ---------------------------------------------------------------------------- #
 
 # Use setwd function to set working directory to the folder containing files
-# downloaded from the Open Science Framework (https://osf.io/jp5ws/).
+# downloaded from the Open Science Framework (https://osf.io/jp5ws/)
 
 # ---------------------------------------------------------------------------- #
 # Import data ----
 # ---------------------------------------------------------------------------- #
 
 # Import partially cleaned mental health history data, remove first column "X",
-# rearrange columns, and convert character columns to factors.
+# rearrange columns, and convert character columns to factors
 
 mentalHealthHx <- read.csv("./Data/Temp/mentalHealthHxTbl3.csv")
 
@@ -55,95 +55,156 @@ mentalHealthHx <-
                      "changeHelp", "other_HelpChange", 
                      "timeOnPage", "date", "id")]
 
-# str(mentalHealthHx)
 mentalHealthHx[sapply(mentalHealthHx, is.character)] <-
   lapply(mentalHealthHx[sapply(mentalHealthHx, is.character)], as.factor)
-# str(mentalHealthHx)
 
-# Import sample data and restrict mental health history data to 958 ITT subjects.
+# Import sample data and restrict mental health history data to 958 ITT subjects
 
 p <- read.csv("./Data/Clean/FTmainAnalysisSamples.csv")
 
 ITT <- subset(p, ittSample == 1)
 completers <- subset(p, txCompSample == 1)
 
-mentalHealthHx.itt <- 
+mentalHealthHx_itt <- 
   mentalHealthHx[mentalHealthHx$participantId %in% ITT$participantId, ]
 
 # Order condition levels based on mental health history table in manuscript and
 # verify number of ITT subjects in each condition
 
-# levels(mentalHealthHx.itt$condition)
-mentalHealthHx.itt$condition <- factor(mentalHealthHx.itt$condition,
-         levels(mentalHealthHx.itt$condition)[c(5, 4, 1, 2, 3)])
+mentalHealthHx_itt$condition <- factor(mentalHealthHx_itt$condition,
+         levels(mentalHealthHx_itt$condition)[c(5, 4, 1, 2, 3)])
 
-# table(mentalHealthHx.itt$condition)
-group.size <- summary(as.factor(mentalHealthHx.itt$condition))
+group.size <- summary(as.factor(mentalHealthHx_itt$condition))
 
 # ---------------------------------------------------------------------------- #
-# Clean free responses following "Other" ----
+# Clean selected variables based on free responses ----
 # ---------------------------------------------------------------------------- #
 
-# TODO: Current diagnoses
+# Change selected variables from factors to characters
 
-mentalHealthHx.itt[grepl("Other", mentalHealthHx.itt$disorders), "other_Desc"]
+mentalHealthHx_itt$disorders <- as.character(mentalHealthHx_itt$disorders)
+mentalHealthHx_itt$pastDisorders <- as.character(mentalHealthHx_itt$pastDisorders)
+mentalHealthHx_itt$help <- as.character(mentalHealthHx_itt$help)
+mentalHealthHx_itt$pastHelp <- as.character(mentalHealthHx_itt$pastHelp)
 
-# [10] BPD = personality
-# [16] PMDD = depression
-# [20] Schizoaffective Disorder/ Bipolar 1 with Psychotic features = 
-#        schiz AND bipolar
-# [24] acquired brain injury = dem
-# [35] CPTSD = PSTD
-# [45] Dysthymia, seasonal affective disorder and substance abuse = 
-#        depression AND subst
-# [49] CPTSD = PSTD
-# [50] depression = depression
-# [51] mood disorder and depression with anxiety = depression
+# The following variables were cleaned with a fairly strict interpretation of the
+# available response categories (e.g., for "disorders" variable, "cyclothymia" was 
+# not considered "bipolar disorder" because it is not strictly bipolar disorder;
+# for "help" variable, "therapy" was not considered "LMHC" because psychiatrists,
+# for example, are another response option and can provide therapy too)
 
+# Current diagnoses ("disorders")
 
+View(mentalHealthHx_itt[grepl("Other", mentalHealthHx_itt$disorders) |
+                          mentalHealthHx_itt$other_Desc != "", ])
 
+# For participant 458, given "other_Desc" of "PMDD", replace "Other" in "disorders"
+# with "depression" (not already there)
+mentalHealthHx_itt[mentalHealthHx_itt$participantId == 458, "disorders"] <-
+  sub("Other", "depression", 
+    mentalHealthHx_itt[mentalHealthHx_itt$participantId == 458, "disorders"])
 
+# For participant 660, given "other_Desc" of "Schizoaffective Disorder/ Bipolar 1 
+# with Psychotic features", replace "Other" in "disorders" with "schiz" (not already 
+# there) to accompany "bipolar" (already there)
+mentalHealthHx_itt[mentalHealthHx_itt$participantId == 660, "disorders"] <-
+  sub("Other", "schiz", 
+      mentalHealthHx_itt[mentalHealthHx_itt$participantId == 660, "disorders"])
 
-# TODO: Past diagnoses
+# For participant 729, given "other_Desc" of "acquired brain injury", replace 
+# "Other" in "disorders" with "dem" (not already there)
+mentalHealthHx_itt[mentalHealthHx_itt$participantId == 729, "disorders"] <-
+  sub("Other", "dem", 
+      mentalHealthHx_itt[mentalHealthHx_itt$participantId == 729, "disorders"])
 
-mentalHealthHx.itt[grepl("Other", mentalHealthHx.itt$pastDisorders), "other_DescNo"]
+# For participant 1021, given "other_Desc" of "Dysthymia, seasonal affective 
+# disorder and substance abuse", replace "Other" in "disorders" with "depression"
+# and "subst" (not already there)
+mentalHealthHx_itt[mentalHealthHx_itt$participantId == 1021, "disorders"] <-
+  sub("u'Other'", "u'depression', u'subst'", 
+      mentalHealthHx_itt[mentalHealthHx_itt$participantId == 1021, "disorders"])
 
+# For participant 1151, given "other_Desc" of "depression", replace "Other" in 
+# "disorders" with "depression" (not already there)
+mentalHealthHx_itt[mentalHealthHx_itt$participantId == 1151, "disorders"] <-
+  sub("Other", "depression", 
+      mentalHealthHx_itt[mentalHealthHx_itt$participantId == 1151, "disorders"])
 
+# For participant 1162, given "other_Desc" of "mood disorder and depression with
+# anxiety", replace "Other" in "disorders" with "depression" (not already there)
+mentalHealthHx_itt[mentalHealthHx_itt$participantId == 1162, "disorders"] <-
+  sub("Other", "depression", 
+      mentalHealthHx_itt[mentalHealthHx_itt$participantId == 1162, "disorders"])
 
+# Past diagnoses ("pastDisorders")
 
+View(mentalHealthHx_itt[grepl("Other", mentalHealthHx_itt$pastDisorders) |
+                          mentalHealthHx_itt$other_DescNo != "", ])
 
-# Current help
+# For participant 628, given "other_DescNo" of "Postpartum depression", replace 
+# "Other" in "pastDisorders" with "depression" (not already there)
+mentalHealthHx_itt[mentalHealthHx_itt$participantId == 628, "pastDisorders"] <-
+  sub("Other", "depression", 
+      mentalHealthHx_itt[mentalHealthHx_itt$participantId == 628, "pastDisorders"])
 
-mentalHealthHx.itt[grepl("Other", mentalHealthHx.itt$help), "other_HelpCurrent"]
+# For participant 991, given "other_DescNo" of "none", replace "Other" in 
+# "pastDisorders" with "noDiagnosis"
+mentalHealthHx_itt[mentalHealthHx_itt$participantId == 991, "pastDisorders"] <-
+  sub("Other", "noDiagnosis", 
+      mentalHealthHx_itt[mentalHealthHx_itt$participantId == 991, "pastDisorders"])
 
+# Current help ("help")
 
+View(mentalHealthHx_itt[grepl("Other", mentalHealthHx_itt$help) |
+                          mentalHealthHx_itt$other_HelpCurrent != "", ])
 
+# For participant 277, given "other_HelpCurrent" containing "mental health group", 
+# replace "Other" in "help" with "support_group"
+  mentalHealthHx_itt[mentalHealthHx_itt$participantId == 277, "help"] <-
+  sub("Other", "support_group", 
+      mentalHealthHx_itt[mentalHealthHx_itt$participantId == 277, "help"])
 
+# For participant 626, given "other_HelpCurrent" of "On prescription medicine", 
+# replace "Other" in "help" with "medicine"
+mentalHealthHx_itt[mentalHealthHx_itt$participantId == 626, "help"] <-
+  sub("Other", "medicine", 
+      mentalHealthHx_itt[mentalHealthHx_itt$participantId == 626, "help"])
 
-# Past help
+# Past help ("pastHelp")
 
-mentalHealthHx.itt[grepl("Other", mentalHealthHx.itt$pastHelp), "other_HelpPast"]
+View(mentalHealthHx_itt[grepl("Other", mentalHealthHx_itt$pastHelp) |
+                          mentalHealthHx_itt$other_HelpPast != "", ])
 
+# No changes
 
+# ---------------------------------------------------------------------------- #
+# Export partially cleaned CSV file ----
+# ---------------------------------------------------------------------------- #
 
-
+write.csv(mentalHealthHx_itt, file = "./Data/Temp/mentalHealthHx_itt.csv")
 
 # ---------------------------------------------------------------------------- #
 # Further clean mental health history data and compute descriptives ----
 # ---------------------------------------------------------------------------- #
 
+# TODO: Waiting on revised script from Jianhui
+
+
+
+
+
 # Current diagnoses
 
-table(mentalHealthHx.itt$disorders)
-table(mentalHealthHx.itt$other_Desc)
+table(mentalHealthHx_itt$disorders)
+table(mentalHealthHx_itt$other_Desc)
 
-sum(grepl("PTSD", mentalHealthHx.itt$disorders)) # Typo in codebook
-sum(grepl("PSTD", mentalHealthHx.itt$disorders)) # Search for "PSTD" instead
+sum(grepl("PTSD", mentalHealthHx_itt$disorders)) # Typo in codebook
+sum(grepl("PSTD", mentalHealthHx_itt$disorders)) # Search for "PSTD" instead
 
-sum(grepl("subst", mentalHealthHx.itt$disorders)) # Not sure why Jianhui has 0
+sum(grepl("subst", mentalHealthHx_itt$disorders)) # Not sure why Jianhui has 0
 
-sum(grepl("other", mentalHealthHx.itt$disorders)) # Typo in codebook
-sum(grepl("Other", mentalHealthHx.itt$disorders)) # Search for "Other" instead
+sum(grepl("other", mentalHealthHx_itt$disorders)) # Typo in codebook
+sum(grepl("Other", mentalHealthHx_itt$disorders)) # Search for "Other" instead
 
 
 
@@ -151,16 +212,16 @@ sum(grepl("Other", mentalHealthHx.itt$disorders)) # Search for "Other" instead
 
 # Past diagnoses
 
-table(mentalHealthHx.itt$pastDisorders)
-table(mentalHealthHx.itt$other_DescNo)
+table(mentalHealthHx_itt$pastDisorders)
+table(mentalHealthHx_itt$other_DescNo)
 
-sum(grepl("PTSD", mentalHealthHx.itt$pastDisorders)) # Typo in codebook
-sum(grepl("PSTD", mentalHealthHx.itt$pastDisorders)) # Search for "PSTD" instead
+sum(grepl("PTSD", mentalHealthHx_itt$pastDisorders)) # Typo in codebook
+sum(grepl("PSTD", mentalHealthHx_itt$pastDisorders)) # Search for "PSTD" instead
 
-sum(grepl("subst", mentalHealthHx.itt$pastDisorders)) # Not sure why Jianhui has 0
+sum(grepl("subst", mentalHealthHx_itt$pastDisorders)) # Not sure why Jianhui has 0
 
-sum(grepl("other", mentalHealthHx.itt$pastDisorders)) # Typo in codebook
-sum(grepl("Other", mentalHealthHx.itt$pastDisorders)) # Search for "Other" instead
+sum(grepl("other", mentalHealthHx_itt$pastDisorders)) # Typo in codebook
+sum(grepl("Other", mentalHealthHx_itt$pastDisorders)) # Search for "Other" instead
 
 
 
@@ -168,14 +229,14 @@ sum(grepl("Other", mentalHealthHx.itt$pastDisorders)) # Search for "Other" inste
 
 # Current help
 
-table(mentalHealthHx.itt$help)
-table(mentalHealthHx.itt$other_HelpCurrent)
+table(mentalHealthHx_itt$help)
+table(mentalHealthHx_itt$other_HelpCurrent)
 
-sum(grepl("school_counselor", mentalHealthHx.itt$help)) # Not sure why Jianhui has 0
+sum(grepl("school_counselor", mentalHealthHx_itt$help)) # Not sure why Jianhui has 0
 
-sum(grepl("religious_leader", mentalHealthHx.itt$help)) # Not sure why Jianhui has 0
+sum(grepl("religious_leader", mentalHealthHx_itt$help)) # Not sure why Jianhui has 0
 
-sum(grepl("support_group", mentalHealthHx.itt$help)) # Not sure why Jianhui has 0
+sum(grepl("support_group", mentalHealthHx_itt$help)) # Not sure why Jianhui has 0
 
 
 
@@ -183,24 +244,20 @@ sum(grepl("support_group", mentalHealthHx.itt$help)) # Not sure why Jianhui has 
 
 # Past help
 
-table(mentalHealthHx.itt$pastHelp)
-table(mentalHealthHx.itt$other_HelpPast)
+table(mentalHealthHx_itt$pastHelp)
+table(mentalHealthHx_itt$other_HelpPast)
 
-sum(grepl("school_counselor", mentalHealthHx.itt$pastHelp)) # Not sure why Jianhui has 0
+sum(grepl("school_counselor", mentalHealthHx_itt$pastHelp)) # Not sure why Jianhui has 0
 
-sum(grepl("religious_leader", mentalHealthHx.itt$pastHelp)) # Not sure why Jianhui has 0
+sum(grepl("religious_leader", mentalHealthHx_itt$pastHelp)) # Not sure why Jianhui has 0
 
-sum(grepl("support_group", mentalHealthHx.itt$pastHelp)) # Not sure why Jianhui has 0
+sum(grepl("support_group", mentalHealthHx_itt$pastHelp)) # Not sure why Jianhui has 0
 
 
 
 
 
 # TODO: Clean other variables (e.g., recode 555)
-
-
-
-
 
 
 
